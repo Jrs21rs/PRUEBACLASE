7229ad16d95948112c1af888840bac859b628c17
Leidy ruz <leidy@gmail.com>
2024-09-26 18:52:06 -0500

    (nuevo): Primer...

---
+++ src/main/java/co/ucentral/escueladebasquet/EscueladebasquetApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.escueladebasquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscueladebasquetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscueladebasquetApplication.class, args);
+	}
+
+}
